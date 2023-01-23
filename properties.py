e01b695543d8736db10602213cebe32a97864c8d
AzzAdri 007 <aazzarellib@gmail.com>
2023-01-23 16:23:45 +0000

    Slack properties added

--- properties.py
+++ properties.py
@@ -1,6 +1,6 @@
 import bpy
 from bpy.types import PropertyGroup
-from bpy.props import IntProperty
+from bpy.props import IntProperty, FloatProperty
 
 class WireProperties(PropertyGroup):
 
@@ -9,4 +9,18 @@ class WireProperties(PropertyGroup):
         description="Value for number of wires in a collection.",
         default = 1,
         min = 1, max = 10
+    )
+
+    arch_slack : FloatProperty(
+        name="Arch Slack",
+        description="Slack of the total wire.",
+        default = .5,
+        min = 0., max = 1.
+    )
+
+    wire_slack : FloatProperty(
+        name="Segment Slack",
+        description="Slack of segment (how loose is the wire).",
+        default = .5,
+        min = 0., max = 1.
     )
